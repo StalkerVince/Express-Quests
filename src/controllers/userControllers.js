6c2073f53d0a24fa7738afc6457526639ab5fb9b
Thierry Vicente <119328995+StalkerVince@users.noreply.github.com>
2023-11-29 11:49:30 +0100

    finished

---
+++ src/controllers/userControllers.js
@@ -0,0 +1,39 @@
+const database = require("../../database");
+
+const getUsers = (req, res) => {
+  database
+
+    .query("SELECT * FROM users")
+
+    .then(([users]) => {
+      res.json(users);
+    })
+    .catch((err) => {
+      console.error(err);
+
+      res.sendStatus(500);
+    });
+};
+
+const getUserById = (req, res) => {
+  const id = parseInt(req.params.id, 10);
+
+  database
+    .query("SELECT * FROM users WHERE id = ?", [id])
+    .then(([users]) => {
+      if (users[0] != null) {
+        res.json(users[0]);
+      } else {
+        res.sendStatus(404);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
+module.exports = {
+  getUsers,
+  getUserById,
+};
