58247253f403321f6ce28feddc89854a31f08a3d
Thierry Vicente <119328995+StalkerVince@users.noreply.github.com>
2023-12-01 10:21:51 +0100

    Put is working

--- src/app.js
+++ src/app.js
@@ -13,10 +13,14 @@ app.get("/api/movies/:id", movieControllers.getMovieById);
 
 app.post("/api/movies", movieControllers.postMovie);
 
+app.put("/api/movies/:id", movieControllers.updateMovie);
+
 // Routes Users
 app.get("/api/users", userControllers.getUsers);
 app.get("/api/users/:id", userControllers.getUserById);
 
 app.post("/api/users", userControllers.postUser);
+
+app.put("/api/users/:id", userControllers.updateUser);
 
 module.exports = app;
